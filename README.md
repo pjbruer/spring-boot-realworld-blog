17542f3a016a064d01e9ba84c055c718887fb123
Jacob Bruér <bruer.pj@gmail.com>
2020-11-15 15:54:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ![RealWorld Example App using Spring and Mybatis](realworld-logo.png)
+
+> ### Spring boot + MyBatis codebase containing real world examples (CRUD, auth, advanced patterns, etc) that adheres to the [RealWorld](https://github.com/gothinkster/realworld-example-apps) spec and API.
---
+++ src/main/java/com/pjbruer/springbootrealworldblog/SpringBootRealworldBlogApplication.java
@@ -0,0 +1,13 @@
+package com.pjbruer.springbootrealworldblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRealworldBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRealworldBlogApplication.class, args);
+	}
+
+}
