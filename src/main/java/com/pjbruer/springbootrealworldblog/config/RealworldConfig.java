5511606936a406f5e9114e8497d5bfdf2dda3ec1
Jacob Bruér <bruer.pj@gmail.com>
2020-11-17 20:42:55 +0100

    added project config file

---
+++ src/main/java/com/pjbruer/springbootrealworldblog/config/RealworldConfig.java
@@ -0,0 +1,29 @@
+package com.pjbruer.springbootrealworldblog.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+// TODO - utilize environment variables?
+// TODO - @EnableRetry? - Spring retry feature
+
+@Configuration
+public class RealworldConfig {
+
+    @Profile("local")
+    @Primary
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .username("root")
+                .password("password")
+                .url("jdbc:mysql://localhost:3306/blog_db?createDatabaseIfNotExist=true&useSSL=false")
+                .build();
+    }
+
+}
